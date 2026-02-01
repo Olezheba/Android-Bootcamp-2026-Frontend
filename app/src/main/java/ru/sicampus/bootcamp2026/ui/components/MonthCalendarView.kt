53af4e0e3b382e842ce62aba1c5380435e769d82
Olezheba <215rep612per@gmail.com>
2026-02-02 02:17:41 +0300

    feat: Добавление экрана расписания, недельного календаря

---
+++ app/src/main/java/ru/sicampus/bootcamp2026/ui/components/MonthCalendarView.kt
@@ -0,0 +1,8 @@
+package ru.sicampus.bootcamp2026.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MonthView() {
+
+}
---
+++ app/src/main/java/ru/sicampus/bootcamp2026/ui/screens/TimetableScreen.kt
@@ -0,0 +1,59 @@
+package ru.sicampus.bootcamp2026.ui.screens
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.sicampus.bootcamp2026.ui.components.MonthView
+import ru.sicampus.bootcamp2026.ui.components.WeekView
+
+@RequiresApi(Build.VERSION_CODES.O)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun TimetableScreen() {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    val options = listOf("Неделя", "Месяц")
+
+    Scaffold() {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            SingleChoiceSegmentedButtonRow(
+                modifier = Modifier.padding(50.dp)
+            ) {
+                options.forEachIndexed { index, label ->
+                    SegmentedButton(
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = options.size
+                        ),
+                        onClick = { selectedIndex = index },
+                        selected = index == selectedIndex,
+                        label = { Text(label) }
+                    )
+                }
+            }
+
+            when (selectedIndex) {
+                0 -> WeekView()
+                1 -> MonthView()
+            }
+        }
+    }
+}
